512298384c452e237d5bcdef45af03ea36b65618
Povilas <pbrilius@gmail.com>
2021-01-18 11:54:21 +0200

    Fixed job post controller test.

--- full-stack/tests/Feature/JobPostControllerTest.php
+++ full-stack/tests/Feature/JobPostControllerTest.php
@@ -15,11 +15,10 @@ class JobPostControllerTest extends TestCase
      */
     public function testJobPostController()
     {
-        $response = $this->post('/crawl');
-        // $response = $this->post('/crawl', [
-        //     'URL' => 'https://medium.com/swlh/fun-with-python-3-hacking-instagram-giveaways-35e5b1d51670',
-        //     'CSS3' => 'article.meteredContent',
-        // ]);
+        $response = $this->post('/crawl', [
+            'URL' => 'https://medium.com/swlh/fun-with-python-3-hacking-instagram-giveaways-35e5b1d51670',
+            'CSS3' => 'article.meteredContent',
+        ]);
 
         $response->assertStatus(200);
         $response->assertSeeText('Data inserted:');
